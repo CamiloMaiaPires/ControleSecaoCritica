d59c400ef7ff0edb70552c7195f558ef6138d328
CamiloMaiaPires <63420640+CamiloMaiaPires@users.noreply.github.com>
2022-11-15 23:51:54 -0300

    Add files via upload

---
+++ ex2/ativ2a.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <sys/time.h>
+
+int soma = 0;
+
+void aloca_matriz(int l, int c, int*** grid, int*** newgrid){
+  int i, j;
+  
+  *grid = malloc(l * sizeof(int*));
+  *newgrid = malloc(l * sizeof(int*));
+  #pragma omp parallel private(i,j) shared(grid,newgrid,l,c)
+  {
+  #pragma omp for
+  for(i=0; i<l; i++){
+    (*grid)[i] = malloc(c * sizeof(int));
+    (*newgrid)[i] = malloc(c * sizeof(int));
+  }
+
+  #pragma omp for collapse(2)
+  for(i=0;i<l;i++){
+    for(j=0;j<c;j++){
+      (*grid)[i][j] = 0;
+      (*newgrid)[i][j] = 0;
+    }
+  }
+  }
+}
+
+int getNeighbors(int** grid, int i, int j, int linhas, int colunas) {
+  int vivos = 0,x,y;
+  for(y=i-1; y<=i+1; y++){
+    for(x=j-1; x<=j+1; x++){
+      if(y == -1 && x == -1){
+        if (grid[linhas-1][colunas-1] == 1) { vivos++;}
+      } else if(y == -1 && x >= 0 && x <= colunas-1){
+        if (grid[linhas-1][x] == 1) { vivos++;}
+      } else if(y == -1 && x > colunas-1){
+        if (grid[linhas-1][0] == 1) { vivos++;}
+      } else if(x == -1 && y>=0 && y<=linhas-1){
+        if (grid[y][colunas-1] == 1) { vivos++;}
+      } else if(x == -1 && y > linhas-1){
+        if (grid[0][colunas-1] == 1) { vivos++;}
+      } else if(y > linhas-1 && x >=0 && x <= colunas-1){
+        if (grid[0][x] == 1) { vivos++;}
+      } else if(y > linhas-1 && x > colunas-1){
+        if (grid[0][0] == 1) { vivos++;}
+      } else if(x > colunas-1 && y >=0 && y <= linhas-1){
+        if (grid[y][0] == 1) { vivos++;}
+      } else{
+        if (grid[y][x] == 1) { vivos++;}
+      }
+    }
+  }
+  if(grid[i][j] == 1){ vivos--;}
+  return vivos;
+}
+
+void nova_geracao(int*** grid, int*** newgrid, int linhas, int colunas){
+  int vivos,i,j;
+  #pragma omp parallel private(i,j,vivos) shared(grid,newgrid,linhas,colunas) 
+  {
+  #pragma omp for collapse(2)
+  for(i=0;i<linhas;i++){
+    for(j=0;j<colunas;j++){
+      vivos = getNeighbors((*grid), i, j, linhas, colunas);
+      if((*grid)[i][j] == 1 && vivos == 2 || vivos == 3){
+        (*newgrid)[i][j] = 1;
+      } else if((*grid)[i][j] == 0 && vivos == 3){
+        (*newgrid)[i][j] = 1;
+      } else{
+        (*newgrid)[i][j] = 0;
+      }
+    }
+  }
+  #pragma omp for collapse(2)
+  for(i=0;i<linhas;i++){
+    for(int j=0;j<colunas;j++){
+      (*grid)[i][j] = (*newgrid)[i][j];
+    }
+  }
+  }
+}
+
+int soma_celulas(int*** grid, int linhas, int colunas){
+  int i,j;
+  int nthreads;
+  #pragma omp parallel private(i,j) shared(grid,linhas,colunas,soma)
+  {
+  #pragma omp for collapse(2)
+  for(i=0;i<linhas;i++){
+    for(j=0;j<colunas;j++){
+      #pragma omp critical
+      soma += (*grid)[i][j];
+    }
+  }
+  }
+  return soma;
+}
+
+int main(void) {
+  omp_set_num_threads(8);
+  struct timeval inicio, final;
+  long long tmili;
+  int linhas = 2048;
+  int colunas = 2048;
+  int geracoes = 2000;
+
+  int **grid, **newgrid;
+  aloca_matriz(linhas, colunas, &grid, &newgrid);
+
+  //GLIDER
+  int lin = 1, col = 1;
+  grid[lin  ][col+1] = 1;
+  grid[lin+1][col+2] = 1;
+  grid[lin+2][col  ] = 1;
+  grid[lin+2][col+1] = 1;
+  grid[lin+2][col+2] = 1;
+
+  //R-pentomino
+  lin =10; col = 30;
+  grid[lin  ][col+1] = 1;
+  grid[lin  ][col+2] = 1;
+  grid[lin+1][col  ] = 1;
+  grid[lin+1][col+1] = 1;
+  grid[lin+2][col+1] = 1;
+
+  for(int k=0; k<geracoes; k++){
+    nova_geracao(&grid, &newgrid, linhas, colunas);
+  }
+
+  gettimeofday(&inicio, NULL);
+  int total = soma_celulas(&grid, linhas, colunas);
+  printf("%d\n", total);
+  gettimeofday(&final, NULL);
+  tmili = (int) (1000*(final.tv_sec - inicio.tv_sec) + (final.tv_usec - inicio.tv_usec) / 1000);
+  printf("tempo do trecho de código do somatório de células vivas da última geração - openmp: %lld  ms\n", tmili);
+}
